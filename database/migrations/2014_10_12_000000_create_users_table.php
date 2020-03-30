588f665a7ec6d40f4b923cac273e3e74bcc147c7
heba <hebe10@yahoo.com>
2020-03-30 18:07:22 +0200

    modify the user table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,11 +16,17 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('gender', 100);
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('password_confirmation');
             $table->rememberToken();
             $table->timestamps();
+            $table->date('date_of_birth');
+            $table->string('image');
+            $table->string('mobile_number');
+            $table->string('national_id');
         });
     }
 
