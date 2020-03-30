79b9adb1e26ad1a4e505c0224b0bb73e81bfb64d
heba <hebe10@yahoo.com>
2020-03-30 23:05:37 +0200

    login with token

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,20 +1,43 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use Illuminate\Support\Facades\Auth; 
 use Illuminate\Http\Request;
 use App\User;
+use Validator;
 
 class UserController extends Controller
+
 {
+    public $successStatus = 200;
     public function register(Request $request) 
     { 
-  
+        $validator = Validator::make($request->all(), [ 
+            'name' => 'required', 
+            'email' => 'required|email', 
+            'password' => 'required', 
+            'password_confirmation' => 'required|same:password', 
+        ]);
+if ($validator->fails()) { 
+            return response()->json(['error'=>$validator->errors()], 401);            
+        }
 $input = $request->all(); 
         $input['password'] = bcrypt($input['password']); 
         $user = User::create($input); 
         $success['token'] =  $user->createToken('MyApp')-> accessToken; 
         $success['name'] =  $user->name;
-return response()->json(['success'=>$success]); 
+return response()->json(['success'=>$success],$this-> successStatus); 
+    }
+
+
+    public function login(){ 
+        if(Auth::attempt(['email' => request('email'), 'password' => request('password')])){ 
+            $user = Auth::user(); 
+            $success['token'] =  $user->createToken('MyApp')-> accessToken; 
+            return response()->json(['successdddddddd' => $success], $this-> successStatus); 
+        } 
+        else{ 
+            return response()->json(['error'=>'Unauthorised'], 401); 
+        } 
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -17,6 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-// Route::post('login', 'ApiController@login');//->middleware('verified');
-// Route::post('register', 'ApiController@register');
+
 Route::post('register', 'UserController@register');
+Route::post('login', 'UserController@login');
