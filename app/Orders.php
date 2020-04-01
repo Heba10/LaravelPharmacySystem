840b273889d58024aa6286ce580bf5591a4d8a31
heba <hebe10@yahoo.com>
2020-04-01 21:27:22 +0200

    add order table and the model relation

--- app/Orders.php
+++ app/Orders.php
@@ -4,16 +4,10 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Addresses extends Model
+class Orders extends Model
 {
     protected $fillable = [
-    	'area_id',
-    	'street_name',
-        'building_number',
-        'floor_number',
-        'flat_number',
-        'is_main',
-        'user_id',
+    
     ];
 
     public function user(){
