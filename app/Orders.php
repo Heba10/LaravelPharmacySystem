d637f5661eac1b15b109f994a3f2f1cc00e625d2
heba <hebe10@yahoo.com>
2020-04-01 22:48:05 +0200

    modify orders table

--- app/Orders.php
+++ app/Orders.php
@@ -7,10 +7,18 @@ use Illuminate\Database\Eloquent\Model;
 class Orders extends Model
 {
     protected $fillable = [
-    
+        'delivering_address_id',
+       
+        'is_insured',
+        'status_id',
+        'pharmacy_id',
+        'order_user_id'
     ];
 
     public function user(){
         return $this->belongsTo('App\User');
+    }
+    public function address(){
+        return $this->belongsTo('App\Addresses');
     }
 }
