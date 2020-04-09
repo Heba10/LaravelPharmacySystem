14d3b45c9a9ad5777b58e112560225ad8c10a614
mahmoudelkhadragy <mahmoudelkhadragy38@gmail.com>
2020-04-09 11:27:43 +0200

    add DoctorLoginController

---
+++ app/Http/Controllers/Doctor/Auth/DoctorLoginController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers\Doctor\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Auth\Authenticatable;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+
+class DoctorLoginController extends Controller
+{
+    use AuthenticatesUsers;
+
+    public function __construct()
+    {
+        $this->middleware('guest:doctor')->except('doctorLogout');
+    }
+
+    public function showLoginForm()
+    {
+        return view('auth.doctor.login');
+    }
+
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required|min:6'
+        ]);
+
+        $credentials = [
+            'email' => $request->email,
+            'password' => $request->password
+        ];
+
+        if( Auth::guard('doctor')->attempt($credentials, $request->remember) )
+        {
+            //if successful , then redirect to thier intended location
+            return redirect()->intended(route('doctors.index'));
+
+        }
+
+        //if unsuccessful , then redirect back to login form
+        return redirect()->back()->withInput($request->only('email', 'remember'));
+
+    }
+
+    public function doctorLogout()
+    {
+        Auth::guard('admin')->logout();
+
+        return redirect('/');
+    }
+    
+}
