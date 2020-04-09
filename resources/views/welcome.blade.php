1bb4db897199e7925d34ff8f8508f0a15cfc6b23
mahmoudelkhadragy <mahmoudelkhadragy38@gmail.com>
2020-04-09 20:30:39 +0200

    remove links from welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -85,7 +85,7 @@
     </head>
     <body>
         <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
+            {{-- @if (Route::has('login'))
                 <div class="top-right links">
                     @auth
                         <a href="{{ url('/home') }}">Home</a>
@@ -97,7 +97,7 @@
                         @endif
                     @endauth
                 </div>
-            @endif
+            @endif --}}
 
             <div class="content">
                 <div class="row">
