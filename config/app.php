395aa1a69ee4b39790997288faf3d813e2d07347
mahmoudelkhadragy <mahmoudelkhadragy38@gmail.com>
2020-04-09 11:30:26 +0200

    add yajra

--- config/app.php
+++ config/app.php
@@ -175,6 +175,8 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         Laravel\Passport\PassportServiceProvider::class,
+        
+        Yajra\DataTables\DataTablesServiceProvider::class,
 
         // Spatie\Permission\PermissionServiceProvider::class,
 
