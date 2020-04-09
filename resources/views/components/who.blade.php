dbc60d7d20431535efcf5fb3c1f7854ceb59173d
mahmoudelkhadragy <mahmoudelkhadragy38@gmail.com>
2020-04-09 20:32:11 +0200

    fix links

--- resources/views/components/who.blade.php
+++ resources/views/components/who.blade.php
@@ -2,27 +2,49 @@
 
 @if ( Auth::guard('web')->check() )
 
-    <a class="nav-link" href="{{ route('admin.logout') }}">
+    {{-- <a class="nav-link" href="{{ route('user.logout') }}">
         {{ __('Logout') }}
-    </a>  
+    </a>   --}}
+    <a href="{{ route('user.logout') }}" class="dropdown-item">
+        Logout as user
+    </a>
 
 @endif
 
 
 @if ( Auth::guard('admin')->check() )
     
-    <a class="nav-link" href="{{ route('admin.logout') }}">
+    {{-- <a class="nav-link" href="{{ route('admin.logout') }}">
         {{ __('Logout') }}
+    </a> --}}
+
+    <a href="{{ route('admin.logout') }}" class="dropdown-item">
+        Logout as admin
     </a>
 
 @endif
 
 
 @if ( Auth::guard('doctor')->check() )
-    Admin 
+
+    {{-- <a class="nav-link" href="{{ route('doctors.logout') }}">
+        {{ __('Logout') }}
+    </a> --}}
+
+    <a href="{{ route('doctors.logout') }}" class="dropdown-item">
+        Logout as doctor
+    </a>
+
 @endif
 
 
 @if ( Auth::guard('pharmacy')->check() )
-    Admin 
+
+    {{-- <a class="nav-link" href="{{ route('pharmacy.logout') }}">
+        {{ __('Logout') }}
+    </a> --}}
+
+    <a href="{{ route('pharmacy.logout') }}" class="dropdown-item">
+        Logout as pharmacy
+    </a>
 @endif
