5cbc3300aac20e157141bec9f330aed5edd48bf8
mahmoudelkhadragy <mahmoudelkhadragy38@gmail.com>
2020-04-09 16:24:06 +0200

    edit who component

--- resources/views/components/who.blade.php
+++ resources/views/components/who.blade.php
@@ -1,7 +1,28 @@
+
+
 @if ( Auth::guard('web')->check() )
-    Normal User  
+
+    <a class="nav-link" href="{{ route('admin.logout') }}">
+        {{ __('Logout') }}
+    </a>  
+
 @endif
 
+
 @if ( Auth::guard('admin')->check() )
+    
+    <a class="nav-link" href="{{ route('admin.logout') }}">
+        {{ __('Logout') }}
+    </a>
+
+@endif
+
+
+@if ( Auth::guard('doctor')->check() )
+    Admin 
+@endif
+
+
+@if ( Auth::guard('pharmacy')->check() )
     Admin 
 @endif
