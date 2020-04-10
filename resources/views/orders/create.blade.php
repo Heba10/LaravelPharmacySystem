08a6d6b59c3e7aec3ff1220f7103af9876ebf5b7
heba <hebe10@yahoo.com>
2020-04-10 09:25:18 +0200

    create order view

--- resources/views/orders/create.blade.php
+++ resources/views/orders/create.blade.php
@@ -11,7 +11,7 @@
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <h1 class="m-0 text-dark">Doctors Page</h1> <!-- here we can add title to every page -->
+            <h1 class="m-0 text-dark">Create New Order</h1> <!-- here we can add title to every page -->
           </div><!-- /.col -->
           {{-- <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
@@ -25,53 +25,101 @@
     <!-- /.content-header -->
    
     <!-- Main content -->
-    <form method="POST" action="{{route('orders.store')}}">
-    @csrf
+  
   
 
-    <div class="form-group">
-      <!-- put name to can use it in controler function store -->
-    <label for="exampleInputPassword1">User Name</label>
-    <select name="pharamcy_id" class="form-control">
-        @foreach($users as $user)  
-          <option value="{{$user->id}}">{{$user->name}}</option>
-        @endforeach
-        </select>
-        </div>
-
-
-        <div class="form-group">
-      <label >Delivering Address</label>
-      <input name="delivering_address_id" type="text" class="form-control" aria-describedby="emailHelp">
-    </div>
-  
-    <div class="form-group">
-      <label >Doctor Name</label>
-      <input name="doctor_id" type="text" class="form-control" aria-describedby="emailHelp">
-    </div>
-  
-    <div class="form-group">
-      <label >Is Insured</label>
-      <input name="is_insured" type="text" class="form-control" aria-describedby="emailHelp">
-    </div>
-
-
-    <div class="form-group">
-      <label >Status</label>
-      <input name="status_id" type="text" class="form-control" aria-describedby="emailHelp">
-    </div>
-
-
-
-         <button type="submit" class="btn btn-primary">Submit</button>
-  </form>
-    
-
-
-
-      </div><!-- /.container-fluid -->
-    </div>
-    <!-- /.content -->
+<form method="POST" action="{{route('orders.store')}}">
+@csrf
+<div class=" px-5">
+    <div class="container px-5 Container">
+    <div class="row medicineRow">
+        <div class="col-4 Container">
+    <label for="exampleFormControlSelect1">User</label>
+    <select class="form-control mb-4" id="userSelect" name="order_user_id">
+      <option ></option>
+      @foreach ($users as $user)
+       <option value="{{$user->id}}">{{$user->name}}</option>
+      @endforeach
+    </select>
   </div>
-  <!-- /.content-wrapper -->
+ 
+  <div class="container px-5 Container">
+  
+        <label for="exampleFormControlInput1">Medicine Name</label>
+        <select  class="form-control mb-4 medicineNameSelect">
+          @foreach ($medicines as $medicine)
+          <option ></option>
+           <option value="{{$medicine->name}}">{{$medicine->name}}</option>
+          @endforeach
+        </select>
+      </div>
+      <div class="container px-5 Container">
+        <label for="">Medicine Type</label>
+        <select   class="form-control mb-4 medicineTypeSelect">
+          <option ></option>
+          @foreach ($medicines as $medicine)
+           <option value="{{$medicine->type}}">{{$medicine->type}}</option>
+          @endforeach
+        </select>
+      </div>
+      <div class="container px-5 Container">
+        <label for="">Quantity</label>
+      <input type="number" class="form-control mb-4 quantity" >
+      </div>
+      <div class="container px-5 Container">
+        <label for="">Price</label>
+      <input type="number" class="form-control mb-4 price" >
+      </div>
+      
+        <button class="btn btn-success add" id="addMedBtn" type="button"  style="width:80%;">add new medicine</button>
+        <button class="btn btn-danger delete" type='button'>delete the medicine</button>
+      
+    </div>
+    
+    
+  </div>
+
+  <div class="form-group px-5">
+    <label for="" >is insured</label>
+    <input type="checkbox" name="is_insured" value='1'>
+  </div>
+  <div class="form-group px-5">
+    <label for="exampleFormControlInput1">Statues</label>
+    <select name="status_id"  class="form-control  @auth('admin') {{ 'disabled' }} @endauth" >
+      @foreach ($statuses as $s=>$v)
+      <option value={{$v}} >{{$s}}</option>
+      @endforeach
+
+    </select>
+  </div>
+
+  <div class="form-group">
+    <button type="submit" class="btn btn-success d-block mx-auto" style="width:80%;">Add order and calculate the price</button>
+  </div>
+
+</form>
+<div class="container">
+  <div class="row text-left">
+    <div class="col">
+      <p>Total Price: <span id="totalPrice"></span></p>
+    </div>
+  </div>
+</div>
+
+<select class="form-control mb-4 medData d-none">
+  <option ></option>
+  @foreach ($medicines as $medicine)
+   <option value="{{$medicine->name}}">{{$medicine->name}}</option>
+  @endforeach
+</select>
+
+<select  class="form-control mb-4 typeData d-none">
+  <option ></option>
+  @foreach ($medicines as $medicine)
+   <option value="{{$medicine->type}}">{{$medicine->type}}</option>
+  @endforeach
+</select>
+
+
+
   @endsection
